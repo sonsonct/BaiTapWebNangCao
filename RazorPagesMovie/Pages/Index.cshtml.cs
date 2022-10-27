f4fe89ea8d6c3c60fa8d49f6389acfddfa38f8bf
phung thai son <sondzvodoi@gmail.con>
2022-10-27 19:42:43 +0700

    bai tap

---
+++ RazorPagesMovie/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+        private readonly ILogger<IndexModel> _logger;
+        public void OnGet()
+        {
+
+        }
+    }
+}
